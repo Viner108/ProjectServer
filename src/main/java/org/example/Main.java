df61cc110e057fba05d893de3e769f541ac84ad6
Viner108 <andrukovichl@mail.ru>
2023-12-01 13:18:55 +0300

    mvn install

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("ProjectServer");
