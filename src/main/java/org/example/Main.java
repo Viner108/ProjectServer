5afda7ce24cec287872e5e4d32934375a71fcac9
Viner108 <andrukovichl@mail.ru>
2023-12-01 15:13:07 +0300

    socket

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+    //    private static BufferedReader in; // поток чтения из сокета
+//    private static BufferedWriter out; // поток записи в сокет
+
+    private static ServerSocket server;
+    private static ObjectInputStream in;
+    private static ObjectOutputStream out;
+
     public static void main(String[] args) {
-        System.out.println("ProjectServer");
+        try {
+            try {
+                server = new ServerSocket(4004);
+                System.out.println("Сервер запущен!");
+                Socket socket1 = server.accept();
+                try {
+                    System.out.println("готовим вх и вых");
+                    in = new ObjectInputStream(socket1.getInputStream());
+                    out = new ObjectOutputStream(socket1.getOutputStream());
+                } finally {
+                    socket1.close();
+                    in.close();
+                    out.close();
+                }
+            } finally {
+                System.out.println("Сервер закрыт!");
+                server.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 }
